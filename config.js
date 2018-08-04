ab44256fc53c1f7b33378b1b57a4f3b8c197a33e
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-08-04 01:22:45 -0600

    Initial Config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	//token generation field for your app
+    FB_PAGE_TOKEN: 'EAAWZA4bD4cbQBAOKHALYC9q2F2RNLJAF5iMxE5tNqzUfgX9k4bh0coMOFPhtmYdalw2CT4HVm36s4DiYA2ca8OJROLu5Sj07JUWBKpMLQ9Am5lXIO9dtg0OGNfLMokF6x3Mm5mfLhIkUqiIQNWXd1LKcANPEmXIZCOjUHVFAZDZD',
+    //page subscription verify token
+    FB_VERIFY_TOKEN: 'dangerzone08createdverifytoken',
+    API_AI_CLIENT_ACCESS_TOKEN: 'c4a1b8f3474242e8bce2be097fac0c4f',
+    FB_APP_SECRET: 'f78cb72fb86439dbed0c058a1c493dff',
+    SERVER_URL: "https://fierce-oasis-94543.herokuapp.com/",
+};
